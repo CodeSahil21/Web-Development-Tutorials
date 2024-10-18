f0d921b1028d4f737871043daf448ee055be55a9
CodeSahil21 <sahil.s39026@email.com>
2024-10-18 10:05:45 +0530

    week11 file committed

---
+++ week11/11.1(serverless backend)/my-app2/src/index.ts
@@ -0,0 +1,50 @@
+// import { Hono } from 'hono'
+
+// const app = new Hono()
+
+// app.get('/', async (c) => {
+//   const body = await c.req.json()
+//   console.log(body);
+//   console.log(c.req.header("Authorization"));
+//   console.log(c.req.query("param"));
+
+//   return c.text('Hello Hono!')
+// })
+
+// export default app
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx';
+
+
+//c means context
+const app = new Hono()
+
+async function authMiddlware(c:any, next:any){
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+}
+app.use(authMiddlware);
+app.get('/', async (c) => {
+  const body = await c.req.parseBody()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({msg: "as"})
+})
+
+export default app
+
+ 
+
+
+
+
+
+
+
+
